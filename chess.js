47b4b2e1338d749ff27276018309f33397921875
massoncorlette <massoncorlette07@gmail.com>
2024-10-19 23:13:48 -0400

    new approach, every array in list will be a path, opposed to potential moves, don't see a way to get shortest path with the last approach

--- chess.js
+++ chess.js
@@ -7,7 +7,7 @@ function knightMoves(start,end) {
   function knightMoveErrorHandler(x,y) {
     let move = []
     if (!(x < 0 || x > 7 || y < 0 || y > 7)) {
-      move.push(x,y);
+      move.push([start[0],start[1]],[x,y]);
       return move;
     }
   };
@@ -22,45 +22,19 @@ function knightMoves(start,end) {
       [moveXY[0] - 2, moveXY[1] - 1],
       [moveXY[0] - 1, moveXY[1] + 2],
       [moveXY[0] - 1, moveXY[1] - 2],
-    ];
+    ]
 
-    // getting the valid moves
-    let results = [];
     currentMoves.forEach((move) => {
       let result = knightMoveErrorHandler(move[0], move[1]);
       if (result) {
-        results.push(result);
+        adjacencyListOfMoves.push(result);
       }
     });
 
-    if (results.length !== 0) {
-      return results;
-    }  
-
   };
   // may need to have the starting vertex for every array the given move taken
-  let movesQue = filterVertices(start);
-  console.log(movesQue);
-
-  while (movesQue.length !== 0) {
-    let currentQue = movesQue.shift();
-
-    let nextMoves = filterVertices(currentQue);
-
-    for (let i=0;i<nextMoves.length;i++) {
-      movesQue.push(nextMoves[i]);
-    }
-
-    adjacencyListOfMoves.push(nextMoves);
-
-    for (let i=0;i<nextMoves.length;i++) {
-      if (nextMoves[i][0] === end[0] && nextMoves[i][1] === end[1]) {
-        adjacencyListOfMoves.push(nextMoves);
-        return adjacencyListOfMoves;
-      }
-    }
-  }
-    
+  filterVertices(start);
+  return adjacencyListOfMoves;
 };
 
 console.log(knightMoves([3,3], [4,3]));
